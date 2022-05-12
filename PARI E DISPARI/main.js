877ba69381c106ffadb526cdc1175075105d7be3
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-05-12 14:52:01 +0200

    pari o dispari

---
+++ PARI E DISPARI/main.js
@@ -0,0 +1,26 @@
+// utente sceglie pari o dispari
+const pariDispari = prompt('scegli pari o dispari');
+console.log(pariDispari);
+// utente numero da 1 a 5
+let sceltaNum = parseInt(prompt('scegli un numero da 1 a 5'));
+console.log(sceltaNum);
+// generare numero random da 1 a 5 per il computer
+
+genRandNum(5, 1)
+
+
+function genRandNum(maxRange, minRange) {
+    let randNum = Math.floor((Math.random(maxRange - minRange) * 5) + 1);
+    console.log(randNum);
+    let somma = sceltaNum + randNum;
+    console.log(somma);
+    if (somma %2 == 1) {
+        console.log('Numero dispari');
+    }
+
+    else {
+        console.log('Numero pari!');
+    }
+}
+
+
