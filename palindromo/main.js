65f0f6214ee56ad53bf3d5dd59f0d37b98220c09
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-05-12 17:48:53 +0200

    palindromo

--- palindromo/main.js
+++ palindromo/main.js
@@ -2,9 +2,9 @@
 let inputUtente = prompt('inserisci una parola');
 console.log(inputUtente);
 // creare una funzione per verificare se la parola scritta può essere
-
-function parolapalindroma() {
-    if (inputUtente.reverse() == inputUtente) {
+parolapalindroma (inputUtente.reverse())
+function parolapalindroma(reverse) {
+    if (reverse == inputUtente) {
         console.log(`${inputUtente} è palindromo`);
     }
 }
